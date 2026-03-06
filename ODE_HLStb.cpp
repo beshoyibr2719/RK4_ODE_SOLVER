4ea728fa63d3c3a3f724bf8600b15213d4d8709d
Beshoy <beshoyibrahim1999@gmail.com>
2026-03-06 13:51:07 -0600

    Add files via upload

---
+++ ODE_HLStb.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    double t = 0.0;
+    double y = 0.5;
+    double h = 0.1;
+    int steps = 20;
+
+    for(int i = 0; i < steps; i++)
+    {
+        std::cout << "t = " << t << ", y = " << y << std::endl;
+        y = rk4_step(t, y, h);
+        t += h;
+    }
+    return 0;
+}
